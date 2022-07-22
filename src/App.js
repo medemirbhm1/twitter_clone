e33fa8e05efbdd4177c62185ec2bfb10cf2c8344
Mohamed Emir Bouhamar <notmedemir@pop-os.localdomain>
2022-07-22 09:57:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const App = () => {
+
+  return (
+    <div className="app">
+      <React.StrictMode>
+        
+      </React.StrictMode>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
